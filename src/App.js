881335bfee612826aecd301c766c7ab9bb535fa1
alize.etchegorry <alize.etchegorry>
2022-09-17 14:53:01 +0200

    installation et réorganisation du template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/global.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header></header>
+      <main className="App-main">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +16,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </main>
+      <footer></footer>
     </div>
   );
 }
